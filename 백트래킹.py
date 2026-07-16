dd12205cb88d55b3e752a3990c4de47fb98da45d
j2tori-design <j2tori@gmail.com>
2026-07-16 20:41:39 +0900

    Update 백트래킹.py

--- 백트래킹.py
+++ 백트래킹.py
@@ -7,6 +7,24 @@ def DFS():
             DFS(next)
 '''
 '''
+# 백트래킹 기본 구조
+def backtracking(depth):
+    # 1. 기저 조건 (종료 조건) 확인
+    if depth == 목표_깊이:
+        # 답을 찾았을 때의 처리 (출력, 저장 등)
+        return
+    
+    # 2. 다음 단계로 나아갈 수 있는 후보군 탐색
+    for next_candidate in candidates:
+        # 3. 유망성 검사 (조건 확인)
+        if is_promising(next_candidate):
+            make_choice(next_candidate)       # 선택 진행 (상태 변화)
+            
+            backtracking(depth + 1)           # 재귀 호출로 다음 단계 탐색
+            
+            undo_choice(next_candidate)       # 되돌아왔을 때 선택 복구 (상태 원상복구)
+'''
+'''
 # 3520. 체커 도전
 N = int(input())
 board = [[0]*(N+1) for _ in range(N+1)]
@@ -75,12 +93,23 @@ def fract(n):
 fract(400)
 done()
 '''
-# 3501. RGB거리
-def RGB(i):
-    if
-    RGB(i-1)
-    RGB(i+1)
-N = int(input())
-cost = [[] for _ in range(N)]
-for i in range(N):
-    cost[i].append(list(map(int, input().split())))
+'''
+# 3009. 부분수열의 합
+count = 0
+def backtrack(n,s,arr,index,result):
+    global count
+    if index == n:
+        return
+    
+    if result + arr[index] == s:
+        count += 1
+    
+    backtrack(n,s,arr,index+1,result+arr[index])
+    backtrack(n,s,arr,index+1,result)
+
+N,S = map(int,input().split())
+arr = list(map(int,input().split()))
+
+backtrack(N,S,arr,0,0)
+print(count)
+'''
